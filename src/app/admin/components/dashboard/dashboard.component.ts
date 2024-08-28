b8e55bb489a2bf6d1a748f8c2de0d9998ec3735e
Maxime <maxime.b2494@gmail.com>
2024-08-28 15:49:23 +0200

    toDo : corriger la bar de recherche du admin dashboard

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../service/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { title } from 'process';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +11,18 @@ import { AdminService } from '../../service/admin.service';
 export class DashboardComponent {
 
   products: any[] = [];
+  searchProductForm!: FormGroup;
 
   constructor(
-    private adminService: AdminService
+    private adminService: AdminService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
     this.getAllProducts();
+    this.searchProductForm = this.fb.group({
+      title: [null, [Validators.required]]
+    });
   }
 
   getAllProducts() {
@@ -25,7 +32,20 @@ export class DashboardComponent {
         element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
         this.products.push(element);
       });
+      console.log(this.products);
     });
   }    
+
+  submitForm(){
+    this.products = [];
+    const name = this.searchProductForm.get('title')!.value;
+    this.adminService.getAllProducts().subscribe(res => {
+      res.forEach((element: { processedImg: string; byteImg: string; }) => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg;
+        this.products.push(element);
+      });
+      console.log(this.products);
+    });
+  }
 
 }

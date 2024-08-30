3e87270f887d22ead369679a018000b33c171e9f
Maxime <maxime.b2494@gmail.com>
2024-08-30 17:06:04 +0200

    feature : admin : track order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { SignupComponent } from './signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { TrackOrderComponent } from './track-order/track-order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    TrackOrderComponent
   ],
   imports: [
     BrowserModule,

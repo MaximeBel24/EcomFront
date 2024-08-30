1e59bd1440cedf22876e49061713151159b5372a
Maxime <maxime.b2494@gmail.com>
2024-08-30 17:49:17 +0200

    feature : admin : analytics

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -9,11 +9,13 @@ import { CouponsComponent } from './components/coupons/coupons.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { PostProductFaqComponent } from './components/post-product-faq/post-product-faq.component';
 import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
 
 const routes: Routes = [
   { path: '', component: AdminComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'category', component: PostCategoryComponent },
+  { path: 'analytics', component: AnalyticsComponent},
   { path: 'product', component: PostProductComponent },
   { path: 'product/:productId', component: UpdateProductComponent},
   { path: 'post-coupon', component: PostCouponComponent },
